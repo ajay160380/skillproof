a2c11181474d7b760e773a896852b3328e0873ef
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-08-04 18:20:32 +0530

    fix: allow port 5174 in CORS settings

--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'http://localhost:5174',
+    'http://127.0.0.1:5174',
     'https://skillproof-eight.vercel.app',
     'https://skillproofi-rouge.vercel.app',
 ]
