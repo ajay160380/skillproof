026451e93b83e94f09ec521dbaf04efbe17b8272
ajay vishwakarma <ajaykumar160380@gmail.com>
2026-08-05 21:06:15 +0530

    docs: add comment for urlpatterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, UserProfileView, CheckUsernameView, PublicProfileView
 
+# URLs for user accounts (registration, login, profile management)
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
